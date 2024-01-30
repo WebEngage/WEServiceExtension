bb2396bff5e8e34b8ff4c949afd8f156fdb692bc
Shubham Naidu <shubham.naidu@webengage.com>
2024-01-30 07:59:31 +0530

    ksa support

--- Sources/WEServiceExtension/Network.swift
+++ Sources/WEServiceExtension/Network.swift
@@ -132,6 +132,8 @@ struct Network {
                 baseURL = "https://c.ir0.webengage.com/tracker"
             } else if environment?.uppercased() == "UNL" {
                 baseURL = "https://c.unl.webengage.com/tracker"
+            } else if environment?.uppercased() == "KSA" {
+                baseURL = "https://c.ksa.webengage.com/tracker"
             }
         }
         
