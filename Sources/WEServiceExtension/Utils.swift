94ccc9e36cbc775d54bf7c7ea7e9c14ef64e1362
Bhavesh Sarwar <bhavesh.sarwar@webengage.com>
2024-04-26 03:51:46 +0530

    version : 1.1.0 updated

--- Sources/WEServiceExtension/Utils.swift
+++ Sources/WEServiceExtension/Utils.swift
@@ -11,7 +11,7 @@ import Foundation
 struct Utils {
     
     /// The version of the service extension.
-    static let WEX_SERVICE_EXTENSION_VERSION = "1.0.0"
+    static let WEX_SERVICE_EXTENSION_VERSION = "1.1.0"
     static var PROXY_URL : String?
     
     /// Get the current time in a formatted string.
